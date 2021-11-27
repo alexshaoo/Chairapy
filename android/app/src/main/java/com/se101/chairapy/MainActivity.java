a2bf47036003cf25d3974a0c00e03cbbac2a05eb
Alex S <qwshao@uwaterloo.ca>
2021-11-27 11:22:14 -0500

    added notification that goes off 3 seconds after button press

--- android/app/src/main/java/com/se101/chairapy/MainActivity.java
+++ android/app/src/main/java/com/se101/chairapy/MainActivity.java
@@ -1,9 +1,15 @@
 package com.se101.chairapy;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -19,23 +25,42 @@ public class MainActivity extends AppCompatActivity {
     //TextView mTextTv;
     ImageButton mVoiceBtn;
 
+    //Receiver for alarms
+    AlarmReceiver receiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createNotificationChannel();
+
         //mTextTv = findViewById(R.id.textTV);
         mVoiceBtn = findViewById(R.id.voiceBtn);
-
 
         mVoiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 speak();
-
             }
         });
 
+        Button notificationButton = findViewById(R.id.notificationButton);
+
+        notificationButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Set reminder", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+            AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+            // get time - repeats every 10 seconds
+            long currentTime = System.currentTimeMillis();
+            long reminderTime = 300 * 10;
+
+            alarmManager.set(AlarmManager.RTC_WAKEUP, currentTime + reminderTime, pendingIntent);
+        });
     }
 
     private void speak() {
@@ -44,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speaketh");
-
 
         try {
             startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
@@ -69,4 +93,24 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+
+    // method for creating notification channel
+    private void createNotificationChannel() {
+
+        //
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            // initializing channel and setting default paramters
+            CharSequence channelName = "reminderChannel";
+            String desc = "Channel for reminders";
+            int priorityLevel = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("notifyMe", channelName, priorityLevel);
+            channel.setDescription(desc);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
